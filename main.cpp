e6f56fc2a499a3fb4d81b25b04f85b8e3fd9d591
Gustavo Bellini <gustavobellini054@gmail.com>
2025-02-18 16:49:29 -0300

    primeira eatapa pokedex

--- main.cpp
+++ main.cpp
@@ -1,8 +1,98 @@
 #include <iostream>
 using namespace std;
 
+//void pra interface 
+void show_menu(){
+
+    cout << "a. Cadastrar cidade; " << endl;
+    cout << "b. Cadastrar estrada;" << endl;
+    cout << "c. Buscar centro pokemon; " << endl;
+    cout << "d. Cadastrar pokemon; " << endl;
+    cout << "e. Remover pokmemon; " << endl;
+    cout << "f. Listar pokemons;" << endl; //por ordem alfabética de nome
+    cout << "g. Listar pokemon; " << endl; //por ordem alfabética de tipo
+    cout << "h. Contar pokemons de cada tipo; " << endl; 
+    cout << "g. Encontrar pokemons proximos; " << endl;
+
+}
+
+void cadastrar_city()
+{
+    cout << "Funionalidade em contrução" << endl;
+}
+
+void cadastrar_road(){
+    cout << "Funionalidade em contrução" << endl;
+}
+
+void centro_poke(){
+    cout << "Funionalidade em contrução" << endl;
+}
+
+void cadastrar_poke(){
+    cout << "Funionalidade em contrução" << endl;
+}
+
+void remover_poke(){
+    cout << "Funionalidade em contrução" << endl;
+}
+
+void listar_poke_nome(){
+    cout << "Funionalidade em contrução" << endl;
+}
+
+void listar_poke_tipo(){
+    cout << "Funionalidade em contrução" << endl;
+}
+
+void contar_poke_tipo(){
+    cout << "Funionalidade em contrução" << endl;
+}
+
+void find_near_poke(){
+    cout << "Funionalidade em contrução" << endl;
+}
+
+
 int main (){
 
+    //declaração de variáveis
+    char opcoes;
+    string cidade;
+
+    //estrutura de repetição da interface
+    while(1){
+        show_menu();
+
+        cin >> opcoes;
+        cin.ignore();
+
+        switch(opcoes){     //switch case pra seleção de opções du user
+            case 'a':
+            {
+                cadastrar_city();
+                break;
+            }
+
+            // case 'b':
+            // {
+            //     //fgsgjsifgdirg
+            // }
+
+            // case 'c':
+            // {
+            //     <
+            // }
+
+            // case 's': // caso queira sair
+            // {
+            //     return 0;
+            // }
+
+        }
+
+
+    }
 
 
 
@@ -10,5 +100,6 @@ int main (){
 
 
 
-    
+
+    return 0;
 }
