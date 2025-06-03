a2949a245ab8157ce736fc6a77e0b35ef6625c58
Gustavo Bellini <gustavobellini054@gmail.com>
2025-06-03 13:48:31 -0300

    entrega 4

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
-#include <locale.h> // Possibilitar a acentuação.
+#include <string>
+#include <algorithm>
+#include <locale.h>
 
 using namespace std;
 
-#define cidade (11) // quantidade de cidades
+#define cidade 11 // quantidade de cidades
 int grafo[cidade][cidade];
 
 struct town
@@ -17,18 +19,31 @@ struct pokemon
 {
     string nome;
     string tipo;
-    int codigo_poke; //numero
-    int loc_X; //localização do pokemon em X
-    int loc_Y; //localização do pokemon em Y
+    int codigo_poke;
+    int loc_X;
+    int loc_Y;
 };
 
-town cadastro[10]; // struct para cadastro de cidades
-// void pra interface de interação com o usuário
+// Vetores globais
+town cadastro[10];
+pokemon pokedex[100]; // Cadastro de pokemons
+int total_pokemons = 0;
+
+// Funções de comparação
+bool compararPorNome(const pokemon &a, const pokemon &b)
+{
+    return a.nome < b.nome;
+}
+
+bool compararPorTipo(const pokemon &a, const pokemon &b)
+{
+    return a.tipo < b.tipo;
+}
+
+// Função para cadastrar cidades
 void cadastrar_city()
 {
-
-    // grafo pro cadastro das cidades
-    int caminho, origem, destino;
+    // int caminho, origem, destino;
     for (int i = 0; i < cidade; i++)
     {
         for (int j = 0; j < cidade; j++)
@@ -36,21 +51,7 @@ void cadastrar_city()
             grafo[i][j] = 90;
         }
     }
-    /*
-    cidade 0 = Indiro Plateau
-    cidade 1 = Viridian City
-    cidade 2 = pallet town
-    cidade 3 = cinnabar island
-    cidade 4 = fuschia city
-    cidade 5 = vermillion city
-    cidade 6 = lavender town
-    cidade 7 = saffron cty
-    cidade 8 = celadon city
-    cidade 9 = cerulean city
-    ciadade 10 = pewter city
 
-    */
-    // ponderando o grafo beleza
     grafo[0][1] = 45;
     grafo[1][0] = 45;
     grafo[2][1] = 1;
@@ -82,26 +83,25 @@ void cadastrar_city()
     grafo[10][9] = 7;
     grafo[9][10] = 7;
 
-
     int i = 0;
     bool quero_cadastrar = 1;
+    cin.ignore();
+
     while (quero_cadastrar)
     {
-        cout << "Digite o nome da cidade: " << endl;
+        cout << "Digite o nome da cidade: ";
         getline(cin, cadastro[i].nome);
-        cout << "\n";
 
-        cout << "Digite o codigo da cidade: " << endl;
+        cout << "Digite o código da cidade: ";
         cin >> cadastro[i].codigo_cidade;
-        cout << "\n";
 
-        cout << "Essa cidade tem um centro pokemon? (1 para sim e 0 para não)" << endl;
+        cout << "Essa cidade tem um centro Pokémon? (1 para sim, 0 para não): ";
         cin >> cadastro[i].centro_poke;
-        cout << "\n";
 
-        cout << "Gostaria de cadastrar uma nova cidade? (1 para sim 0 para não )" << endl;
+        cout << "Gostaria de cadastrar uma nova cidade? (1 para sim, 0 para não): ";
         cin >> quero_cadastrar;
-        cout << "\n";
+        cin.ignore();
+
         i++;
     }
 }
@@ -112,182 +112,223 @@ void buscar_centro_poke()
     int posicao_centro = -1;
     int cod_cidade;
 
-    cout << "Digite o codigo da cidade: " << endl;
+    cout << "Digite o código da cidade: ";
     cin >> cod_cidade;
 
-    for (int j = 0; j < 11; j++)
+    for (int j = 0; j < cidade; j++)
     {
         if (grafo[j][cod_cidade] != -1)
         {
-            if (grafo[j][cod_cidade] <= distancia) {
+            if (grafo[j][cod_cidade] <= distancia)
+            {
                 distancia = grafo[j][cod_cidade];
                 posicao_centro = cod_cidade;
             }
         }
     }
-    cout << "A cidade mais proxima do centro pokemon e: " << cadastro[posicao_centro].nome << " e a distancia e de: " << distancia << "km" << endl;
+
+    cout << "A cidade mais próxima do centro Pokémon é: " << cadastro[posicao_centro].nome
+         << " e a distância é de: " << distancia << " km\n";
 }
 
 void cadastrar_road()
 {
-    cout << "Funcionalidade em construcao" << endl;
+    cout << "Funcionalidade em construção\n";
 }
-
 
 void cadastrar_poke()
 {
-    int i = 0;
     bool quero_cadastrar = 1;
-    pokemon pokemon[10]; // struct para cadastro de pokemons
 
     while (quero_cadastrar)
     {
-        cin.ignore(); // Limpa o buffer de entrada
-        cout << "Digite o nome do Pokemon: " << endl;
-        getline(cin, pokemon[i].nome);
-        cout << "\n";
+        cin.ignore();
 
-        cout << "Digite o tipo: " << endl;
-        getline(cin,pokemon[i].tipo);
-        cout << "\n";
+        cout << "Digite o nome do Pokémon: ";
+        getline(cin, pokedex[total_pokemons].nome);
 
-        cout << "Digite o numero da pokedex do seu pokemon: " << endl;
-        cin >> pokemon[i].codigo_poke;
-        cout << "\n";
+        cout << "Digite o tipo do Pokémon: ";
+        getline(cin, pokedex[total_pokemons].tipo);
 
-        cout << "Digite a localização do pokemon (X, Y): " << endl;
-        cin >> pokemon[i].loc_X;
-        cout << "\n";
-        cin >> pokemon[i].loc_Y;
-        cout << "\n";
+        cout << "Digite o número da Pokédex: ";
+        cin >> pokedex[total_pokemons].codigo_poke;
 
-        cout << "Gostaria de cadastrar um novo Pokemon? (1 para sim 0 para não )" << endl;
+        cout << "Digite a localização (X Y): ";
+        cin >> pokedex[total_pokemons].loc_X >> pokedex[total_pokemons].loc_Y;
+
+        total_pokemons++;
+
+        cout << "Deseja cadastrar outro Pokémon? (1-Sim / 0-Não): ";
         cin >> quero_cadastrar;
-        cin.ignore(); // Limpa o buffer de entrada
-        cout << "\n";
-        i++;
     }
 }
+
 void remover_poke()
 {
-    cout << "Funcionalidade em construcao" << endl;
+    if (total_pokemons == 0)
+    {
+        cout << "Nenhum Pokémon cadastrado.\n";
+        return;
+    }
+
+    cin.ignore();
+    string nome;
+    cout << "Digite o nome do Pokémon a ser removido: ";
+    getline(cin, nome);
+
+    bool encontrado = false;
+
+    for (int i = 0; i < total_pokemons; i++)
+    {
+        if (pokedex[i].nome == nome)
+        {
+            for (int j = i; j < total_pokemons - 1; j++)
+            {
+                pokedex[j] = pokedex[j + 1];
+            }
+            total_pokemons--;
+            encontrado = true;
+            cout << "Pokémon '" << nome << "' removido com sucesso.\n";
+            break;
+        }
+    }
+
+    if (!encontrado)
+    {
+        cout << "Pokémon '" << nome << "' não encontrado.\n";
+    }
 }
 
 void listar_poke_nome()
 {
-    cout << "Funcionalidade em construcao" << endl;
+    if (total_pokemons == 0)
+    {
+        cout << "Nenhum Pokémon cadastrado.\n";
+        return;
+    }
+
+    sort(pokedex, pokedex + total_pokemons, compararPorNome);
+
+    cout << "\nLista de Pokémons ordenados por NOME:\n";
+    for (int i = 0; i < total_pokemons; i++)
+    {
+        cout << "- Nome: " << pokedex[i].nome
+             << ", Tipo: " << pokedex[i].tipo
+             << ", Número: " << pokedex[i].codigo_poke
+             << ", Localização: (" << pokedex[i].loc_X << ", " << pokedex[i].loc_Y << ")\n";
+    }
 }
 
 void listar_poke_tipo()
 {
-    cout << "Funcionalidade em construcao" << endl;
+    if (total_pokemons == 0)
+    {
+        cout << "Nenhum Pokémon cadastrado.\n";
+        return;
+    }
+
+    sort(pokedex, pokedex + total_pokemons, compararPorTipo);
+
+    cout << "\nLista de Pokémons ordenados por TIPO:\n";
+    for (int i = 0; i < total_pokemons; i++)
+    {
+        cout << "- Nome: " << pokedex[i].nome
+             << ", Tipo: " << pokedex[i].tipo
+             << ", Número: " << pokedex[i].codigo_poke
+             << ", Localização: (" << pokedex[i].loc_X << ", " << pokedex[i].loc_Y << ")\n";
+    }
 }
 
 void contar_poke_tipo()
 {
-    cout << "Funcionalidade em construcao" << endl;
+    if (total_pokemons == 0)
+    {
+        cout << "Nenhum Pokémon cadastrado.\n";
+        return;
+    }
+
+    cin.ignore();
+    string tipo;
+    cout << "Digite o tipo de Pokémon a ser contado: ";
+    getline(cin, tipo);
+
+    int contador = 0;
+    for (int i = 0; i < total_pokemons; i++)
+    {
+        if (pokedex[i].tipo == tipo)
+        {
+            contador++;
+        }
+    }
+
+    cout << "Quantidade de Pokémons do tipo '" << tipo << "': " << contador << endl;
 }
 
 void find_near_poke()
 {
-    cout << "Funcionalidade em construcao" << endl;
+    cout << "Funcionalidade em construção\n";
 }
 
 void show_menu()
 {
-
-    cout << "\nOla cacador de pokemons, o que gostaria de acessar? " << endl;
-    cout << "\na. Cadastrar cidade; " << endl;
-    cout << "b. Cadastrar estrada; " << endl;
-    cout << "c. Buscar centro pokemon; " << endl;
-    cout << "d. Cadastrar pokemon; " << endl;
-    cout << "e. Remover pokmemon; " << endl;
-    cout << "f. Listar pokemons; " << endl; // por ordem alfabética de nome
-    cout << "g. Listar pokemon; " << endl;  // por ordem alfabética de tipo
-    cout << "h. Contar pokemons de cada tipo; " << endl;
-    cout << "i. Encontrar pokemons proximos; " << endl;
-    cout << "s. Encerrar pokedex; \n"
-         << endl;
+    cout << "\nOla caçador de Pokémons, o que gostaria de acessar?\n";
+    cout << "a. Cadastrar cidade\n";
+    cout << "b. Cadastrar estrada\n";
+    cout << "c. Buscar centro Pokémon\n";
+    cout << "d. Cadastrar Pokémon\n";
+    cout << "e. Remover Pokémon\n";
+    cout << "f. Listar Pokémons (por nome)\n";
+    cout << "g. Listar Pokémons (por tipo)\n";
+    cout << "h. Contar Pokémons por tipo\n";
+    cout << "i. Encontrar Pokémons próximos\n";
+    cout << "s. Encerrar Pokédex\n";
 }
 
 int main()
 {
     setlocale(LC_ALL, "Portuguese");
 
-    // declaração de variáveis
     char opcoes;
 
-    // estrutura de repetição da interface
-    while (1)
+    while (true)
     {
-
         show_menu();
-
         cin >> opcoes;
-        cout << "\n";
 
         switch (opcoes)
-        { // switch case pra seleção de opções du user
-        case 'a':
         {
+        case 'a':
             cadastrar_city();
             break;
-        }
-
         case 'b':
-        {
             cadastrar_road();
             break;
-        }
-
         case 'c':
-        {
             buscar_centro_poke();
             break;
-        }
-
         case 'd':
-        {
             cadastrar_poke();
             break;
-        }
-
         case 'e':
-        {
             remover_poke();
             break;
-        }
-
         case 'f':
-        {
             listar_poke_nome();
             break;
-        }
-
         case 'g':
-        {
             listar_poke_tipo();
             break;
-        }
-
         case 'h':
-        {
             contar_poke_tipo();
             break;
-        }
-
         case 'i':
-        {
             find_near_poke();
             break;
-        }
-
-        case 's': // caso queira sair
-        {
-            cout << "Programa encerrado. \n"
-                 << endl;
+        case 's':
+            cout << "Programa encerrado.\n";
             return 0;
-        }
+        default:
+            cout << "Opção inválida.\n";
         }
     }
 
